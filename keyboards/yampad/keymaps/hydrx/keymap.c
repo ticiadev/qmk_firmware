c289db52311f333fcc763c0c7110f34d03679bdb
Ticia Francisco <ticiadev@gmail.com>
2025-01-06 20:36:49 -0800

    Append missing comma in layout

--- keyboards/yampad/keymaps/hydrx/keymap.c
+++ keyboards/yampad/keymaps/hydrx/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------'
  */
   [_DEFAULT] = LAYOUT(
-    KC_PMNS,  KC_PSLS,  KC_PAST,   TT(_NAV)
+    KC_PMNS,  KC_PSLS,  KC_PAST,   TT(_NAV),
     KC_P7,    KC_P8,    KC_P9,
     KC_P4,    KC_P5,    KC_P6,     KC_PPLS,
     KC_P1,    KC_P2,    KC_P3,
